8e8e1561ee9463cf1f133444d9f23af847556339
8kosmo <50821549+8kosmo@users.noreply.github.com>
2019-09-11 15:18:33 +0900

    Update MemberLogic.java

--- src/main/java/com/sellfeed/member/MemberLogic.java
+++ src/main/java/com/sellfeed/member/MemberLogic.java
@@ -1,30 +1,69 @@
-package com.sellfeed.account;
+package com.sellfeed.member;
 
+import java.util.List;
 import java.util.Map;
 
-import org.mybatis.spring.SqlSessionTemplate;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sellfeed.account.AccountDao;
+import com.sellfeed.account.AccountLogic;
 
 @Service
-public class AccountDao {
-	Logger logger = LoggerFactory.getLogger(AccountDao.class);
+public class MemberLogic {
+	Logger logger = LoggerFactory.getLogger(MemberLogic.class);
 	@Autowired
-	public SqlSessionTemplate sqlSessionTemplate = null;
+	public MemberDao memberDao = null;
+	@Autowired
+	public AccountLogic accountLogic = null;
 	
-	public void accountIns(Map<String, Object> pMap) {
-		logger.info("=================>accountIns 호출 성공");
-		sqlSessionTemplate.update("accountIns",pMap);
+//	@Autowired
+//	public AccountDao accountDao = null;
+
+	@Transactional(propagation=Propagation.REQUIRES_NEW, rollbackFor= {DataAccessException.class})
+	@Pointcut(value="execution(* com.sellfeed.member.*Logic.*(..)")
+	public void memberIns(Map<String, Object> pMap) {
+		logger.info("=================>memberIns 호출 성공");
+		//트랜잭션처리
+		//servlet-context.xml에 <tx:annotation-driven /> 넣어주기.
+		try {
+			memberDao.memberIns(pMap);
+			accountLogic.accountIns(pMap);
+			
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String login(Map<String, Object> pMap) {
+		logger.info("=================>login 호출 성공");
+		String mem_name="";
+		mem_name = memberDao.login(pMap);
+		return mem_name;
 	}
 
-	public String getAcct_number() {
-		logger.info("=================>getAcct_number 호출 성공");
-		String acct_number = "";
-		acct_number = sqlSessionTemplate.selectOne("getAcct_number");
-		return acct_number;
+	public void memberUpd(Map<String, Object> pMap) {
+		logger.info("=================>memberUpd 호출 성공");
+		//회원정보 수정
+		memberDao.memberUpd(pMap);
 	}
-	
+
+	public void memberDel(Map<String, Object> pMap) {
+		logger.info("=================>memberDel 호출 성공");
+		memberDao.memberDel(pMap);
+	}
+
+	public List<Map<String, Object>> memberList(Map<String, Object> pMap) {
+		logger.info("=================>memberList 호출 성공");
+		List<Map<String,Object>> rList = null;
+		rList = memberDao.memberList(pMap);
+		return rList;
+		
+	}
 }
